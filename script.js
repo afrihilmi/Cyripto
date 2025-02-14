c2a7891bc60ab32ede71b85fddada2a126a3cec9
afrihilmi <43900755+afrihilmi@users.noreply.github.com>
2025-02-14 23:45:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Contoh JavaScript (opsional)
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Landing page loaded!');
+    // Tambahkan logika JavaScript di sini jika diperlukan
+});
